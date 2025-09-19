2481c211e1a3c97a894372c3aa996773fafc762c
Timor20 <timors294@gmail.com>
2025-09-18 19:28:53 -0700

    Contains duplicate

---
+++ Arrays_and_Hashing/Contains_Duplicate.py
@@ -0,0 +1,14 @@
+#   217 - Contains Duplicate
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+# iterate through the array
+# record values in set
+# if the record already exists then return true, else return false
+        duplicate = set()
+        for num in nums:
+            if num in duplicate:
+                return True
+            else:
+                duplicate.add(num)
+        return False
